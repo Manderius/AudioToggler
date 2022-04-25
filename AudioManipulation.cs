848dc6b17d9134a56d38a0703d6089929d296eb3
Manderius <manderiusmail@gmail.com>
2022-04-25 16:41:03 +0200

    Initial commit

---
+++ AudioManipulation.cs
@@ -0,0 +1,34 @@
+﻿namespace AudioToggler
+{
+    enum AudioLevel { HIGH, LOW };
+
+    internal class AudioManipulation
+    {
+        const string ConfigPath = @"C:\Program Files\EqualizerAPO\config\config.txt";
+        const string TEXT_LOW = "-20";
+        const string TEXT_HIGH = "10";
+
+        public static AudioLevel GetAudioLevel() {
+            string[] lines = File.ReadAllLines(ConfigPath);
+            return lines[0].Contains(TEXT_LOW) ? AudioLevel.LOW : AudioLevel.HIGH;
+        }
+
+        public static AudioLevel ToggleAudio()
+        {
+            string[] lines = File.ReadAllLines(ConfigPath);
+            AudioLevel newLevel;
+            if (lines[0].Contains(TEXT_LOW))
+            {
+                lines[0] = lines[0].Replace(TEXT_LOW, TEXT_HIGH);
+                newLevel = AudioLevel.HIGH;
+            }
+            else
+            {
+                lines[0] = lines[0].Replace(TEXT_HIGH, TEXT_LOW);
+                newLevel = AudioLevel.LOW;
+            }
+            File.WriteAllLines(ConfigPath, lines);
+            return newLevel;
+        }
+    }
+}
---
+++ AudioTogglerTrayIcon.cs
@@ -0,0 +1,45 @@
+﻿namespace AudioToggler
+{
+    internal class AudioTogglerTrayIcon : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private Icon iconHigh = new Icon("resources/speakerHigh.ico");
+        private Icon iconLow = new Icon("resources/speakerLow.ico");
+
+        public AudioTogglerTrayIcon()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon();
+
+            trayIcon.DoubleClick += Toggle;
+            trayIcon.MouseClick += Exit;
+
+            SetTrayIconForLevel(AudioManipulation.GetAudioLevel());
+
+            trayIcon.Visible = true;
+        }
+
+        void Toggle(object? sender, EventArgs e)
+        {
+            AudioLevel level = AudioManipulation.ToggleAudio();
+            SetTrayIconForLevel(level);
+        }
+
+        void SetTrayIconForLevel(AudioLevel level)
+        {
+            var icon = level == AudioLevel.HIGH ? iconHigh : iconLow;
+            trayIcon.Icon = icon;
+        }
+
+        void Exit(object? sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button != MouseButtons.Right) return;
+
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
